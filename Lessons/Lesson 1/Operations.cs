43018db61f34980f8de8d3ea90b80036b55317d2
ernestocasanova <ernestonet@msn.com>
2024-09-20 10:42:46 +0100

    Lesson 1 C#, create simple calculator operations, defined initially in Program with CLS, then refactored code to make then better structured, and explored small example of POO with Polymorphism.

---
+++ Lessons/Lesson 1/Operations.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------​
+//    <copyright file="Lesson.cs" company="IPCA">​
+//     Copyright IPCA-EST. All rights reserved.​
+//    </copyright>​
+//    <date>19-09-2024</date>​
+//    <time>21:00</time>​
+//    <version>0.1</version>​
+//    <author>Ernesto Casanova</author>​
+//-----------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    public static class Operations
+    {
+        /// <summary>
+        /// Sum two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>int</returns>
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Sum two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>double</returns>
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Subtract two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>int</returns>
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
---
+++ Lessons/Lesson 1/Program.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------​
+//    <copyright file="Lesson.cs" company="IPCA">​
+//     Copyright IPCA-EST. All rights reserved.​
+//    </copyright>​
+//    <date>19-09-2024</date>​
+//    <time>21:00</time>​
+//    <version>0.1</version>​
+//    <author>Ernesto Casanova</author>​
+//-----------------------------------------------------------------
+using Lesson_1;
+using System;
+using System.Buffers;
+
+namespace Lesson
+{
+    /// <summary>
+    /// Summary description for Lesson.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main function
+        /// </summary>
+        /// <param name="args">string[]</param>
+        static void Main(string[] args)
+        {
+            double x = Operations.Sum(2.0, 3.0);
+            Console.WriteLine($"Sum = {x}");
+
+            int z = Operations.Sum(2, 3);
+            Console.WriteLine($"Sum = {z}");
+        }
+    }
+}
