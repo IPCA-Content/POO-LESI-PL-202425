4c6b33c7e69458108c032ae603106451e6a41938
Ernesto Casanova <ernestonet@msn.com>
2024-10-16 21:47:55 +0100

    Added missing files.

---
+++ Lessons/Utils/Operations.cs
@@ -0,0 +1,20 @@
+﻿namespace Utils;
+
+/// <summary>
+/// Class Operations 
+/// </summary>
+public class Operations
+{
+    /// <summary>
+    /// Calculate Age base on date
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public int Age(DateOnly date)
+    {
+        DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+        int age = today.Year - date.Year;
+        if (today < date.AddYears(-age)) age--;
+        return age;
+    }
+}
